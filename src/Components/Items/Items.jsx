6272c68549fcecda53810ea49990f71fd80d9d74
MGElnaggar <Mostafagamal5827@gmail.com>
2024-10-20 01:56:29 +0300

    commit

--- src/Components/Items/Items.jsx
+++ src/Components/Items/Items.jsx
@@ -32,10 +32,7 @@ const Items = ({ id, imgSrc, name, price }) => {
                 <h3>{name}</h3>
                 <strong>{price}</strong>
                 <button onClick={handleAddToCart}>
-                    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <line x1="9" y1="4.37114e-08" x2="9" y2="18" stroke="white" strokeWidth="2"/>
-                        <line y1="9" x2="18" y2="9" stroke="white" strokeWidth="2"/>
-                    </svg>
+                    +
                 </button>
             </div>
         </div>
